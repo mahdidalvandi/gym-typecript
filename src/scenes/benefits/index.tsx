cac1044528f0d948fb3e3b87a7fee3340707efe1
MahdiDalvandi <118760616+mahdidalvandi@users.noreply.github.com>
2023-03-15 18:14:13 +0330

    Add files via upload

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -40,7 +40,7 @@ const benefits: Array<BenefitsType> = [
 ];
 const Benefits = ({ setSelectedPage }: Props) => {
   return (
-    <section id="benefits" className="mx-auto min-h-full w-5/6 py-20">
+    <section id="benefites" className="mx-auto min-h-full w-5/6 py-20">
       <motion.div
         onViewportEnter={() => setSelectedPage(SelectedPage.Benefites)}
       >
--- src/scenes/navbar/Link.tsx
+++ src/scenes/navbar/Link.tsx
@@ -9,13 +9,13 @@ type Props = {
 
 function Link({ page, selectedPage, setSelectedPage }: Props) {
   const lowerCasePage = page.toLowerCase().replace(/ /g, "") as SelectedPage;
-
+  console.log(lowerCasePage);
   return (
     <AnchorLink
       className={`${
         selectedPage === lowerCasePage
       } ? "text-primary-500" : "" } hover:text transition duration-500 hover:text-primary-300`}
-      href={``}
+      href={`#${lowerCasePage}`}
       onClick={() => setSelectedPage(lowerCasePage)}
     >
       {page}
